c469488162111ec8f8a918f7bd3e793aeffb382c
Xirvin <xander228irvin@gmail.com>
2024-02-24 21:10:09 +1100

    ref(file structure): Refactor file structure to match BL conventions

---
+++ client/config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+GameTypes = {
+    CircleProgress = "CircleProgress",
+    Progress = "Progress",
+    KeyCircle = "KeyCircle",
+    KeySpam = "KeySpam",
+    NumberSlide = "NumberSlide",
+    RapidLines = "RapidLines",
+    CircleShake = "CircleShake",
+}
+
+Config.KeepGameInput = {
+    [GameTypes.CircleProgress] = true,
+    [GameTypes.Progress] = true,
+    [GameTypes.KeyCircle] = false,
+    [GameTypes.KeySpam] = true,
+    [GameTypes.NumberSlide] = true,
+    [GameTypes.RapidLines] = true,
+    [GameTypes.CircleShake] = false,
+}
---
+++ client/events.lua
@@ -0,0 +1,11 @@
+Send = {
+    visible = "ui:visible",
+    start = "ui:start",
+}
+
+Receive = {
+    close = "ui:close",
+    finish = "ui:finish",
+}
+
+
