b1c4c9e14c1aa84f598a01b897c753b052a647f4
Xirvin <xander228irvin@gmail.com>
2024-07-30 12:16:48 +1000

    cleanup(comments): TempInteractListener

--- web/src/utils/interactHandler.ts
+++ web/src/utils/interactHandler.ts
@@ -5,19 +5,7 @@ type InteractListenerType = 'keydown' | 'keyup' | 'keypress' | 'click' | 'moused
 
 export function TempInteractListener(type: InteractListenerType, handler: (data: KeyboardEvent | MouseEvent) => void,
 ): {removeListener: () => void} {
-    // const eventListener = (event: MessageEvent<NuiMessage<T>>) => {
-    //     const { action: eventAction, data } = event.data;
-
-    //     eventAction === action && handler(data);
-    // };
-
-    // const eventListener = (event: KeyboardEvent) => {
-    //     console.log(event);
-    //     handler(event);
-    // }
-
     function removeListener() {
-        // console.log('Removing event listener');
         window.removeEventListener(type, handler);
     }
 
