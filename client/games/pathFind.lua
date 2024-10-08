551b0e79e2672fc11167e1814a2bfc329a8d4036
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-10-08 14:30:08 +0100

    fix: right args

--- client/games/pathFind.lua
+++ client/games/pathFind.lua
@@ -1,16 +1,9 @@
---- KeySpam Game
 ---@param iterations number The amount of iterations to run
----@param difficulty number The difficulty of the game (1-100)
----@return boolean
-local function pathFind(iterations, difficulty)
+---@param config NodeConfig
+local function pathFind(iterations, config)
     local promise = promise:new()
 
-    ---@type DifficultyConfig
-    local config = {
-        difficulty = difficulty or 50,
-    }
-
-    local result = StartGame(GameTypes.PathFind, iterations, config)
+    local result = StartGame(GameTypes.pathFind, iterations, config)
     promise:resolve(result)
 
     return Citizen.Await(promise)
